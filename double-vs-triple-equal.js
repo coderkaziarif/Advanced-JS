fbc42a0978f8193b56ddac30d4e30209293fd9b9
kazi Arif <coderkaziarif@gmail.com>
2022-03-03 12:51:04 +0600

    double vs triple equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,15 @@
+// const first = 2;
+// const second = "2";
+
+// const first = 1;
+// const second = true;
+
+const first = 0;
+const second = false;
+
+if (first === second) {
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");
+}
