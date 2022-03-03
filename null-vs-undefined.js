ba377473aa0627514bd4482f822e7f9e45a08ecc
kazi Arif <coderkaziarif@gmail.com>
2022-03-03 12:31:07 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,12 @@
+let pakhi;
+function add(num1, num2) {
+    console.log(num1 + num2);
+    return
+}
+
+const premik = { name: "Shumit saha", phone: 017872};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
