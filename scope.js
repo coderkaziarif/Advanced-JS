123ed9fa2ac1abce922fade9b115308df35afdfe
kazi Arif <coderkaziarif@gmail.com>
2022-03-03 17:11:20 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,17 @@
+let bonus = 20;
+
+function sum(first, second) {
+    let result = first + second + bonus;
+    // console.log(bonus);
+    if (result > 9) {
+        var mood = "happy"
+        console.log(mood);
+    }
+    console.log(day);
+    let day = "friday"
+    return result
+}
+
+const output = sum(3, 7);
+console.log(output);
+// console.log(result);
