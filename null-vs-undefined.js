e8c07d42af8df9ea992da0b32e27f2a6edddc880
kazi Arif <coderkaziarif@gmail.com>
2022-03-03 12:34:50 +0600

    done

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,12 +1,19 @@
-let pakhi;
-function add(num1, num2) {
-    console.log(num1 + num2);
-    return
+// let pakhi;
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+//     return
+// }
+
+// const premik = { name: "Shumit saha", phone: 017872};
+// console.log(premik.gf);
+
+// let fun = undefined;
+// console.log(fun);
+
+function doSomething(x, y) {
+
+    console.log(y);
+
 }
 
-const premik = { name: "Shumit saha", phone: 017872};
-console.log(premik.gf);
-
-let fun = undefined;
-console.log(fun);
-
+doSomething(32);
